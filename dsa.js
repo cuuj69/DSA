d9e11d226eed00d68ddddd43d8f342fdcb87df5c
cuuj69 <mensahjefferson69@gmail.com>
2023-02-27 14:13:55 +0000

    added bubble sort implementation

--- dsa.js
+++ dsa.js
@@ -192,3 +192,15 @@ function checkObj(obj, checkProp) {
 }
 
 
+function bubbleSort(arr){
+  const n = arr.length;
+
+  for(let i = 0; i < n; i++){
+    for(let j = 0; j < n -i -1; j++){
+      const temp = arr[j];
+      arr[j] = arr[j + 1];
+      arr[j + 1] = temp;
+    }
+  }
+  return arr;
+}
